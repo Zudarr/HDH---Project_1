e614d6c2dcb7fcb4a38888b43a85ebaa02ac26db
doanphuc281201 <ndp281201@gmail.com>
2022-04-28 11:21:01 +0700

    --first commit

--- HDH---Project_1/ReadData.cpp
+++ HDH---Project_1/ReadData.cpp
@@ -42,3 +42,29 @@ string ReadtoString(BYTE* data, string offsetHex, unsigned int bytes)
 	}
 	return result;
 }
+
+uint64_t LE_read_reverse( uint8_t* start_byte, string Hex_offset,unsigned int total )   //tên hàm cũ : ReadIntReverse
+{
+    // truyền vào tổng số byte cần đọc, toàn bộ offset, vị trí bắt đầu => giá trị cần đọc ở dạng decimal
+
+	const int offset_byte = stoi(Hex_offset, nullptr, 16);
+
+	start_byte = start_byte + offset_byte;
+	uint64_t total_byte = 0;
+	int i;
+	for (i = total - 1; i >= 0; i--)
+		total_byte = (total_byte << 8) | start_byte[i];
+
+	return total_byte;
+}
+void print_txt_data( byte* data)  // tên hàm cũ : void printTextData(byte* data, int size);
+{
+	if(data!=nullptr)
+	{
+		output=(char*)data
+		string text(output);
+
+		cout << text;
+	}
+	
+}
--- HDH---Project_1/ReadData.h
+++ HDH---Project_1/ReadData.h
@@ -9,3 +9,4 @@ using namespace std;
 int hexCharToInt(char a);
 string hexToString(std::string str);
 string ReadtoString(BYTE* data, string offsetHex, unsigned int bytes);
+uint64_t LE_read_reverse( uint8_t* start_byte, string Hex_offset,unsigned int total );   //tên hàm cũ : ReadIntReverse
