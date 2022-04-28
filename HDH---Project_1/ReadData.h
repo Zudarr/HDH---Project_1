253fbe3c846c7bf530e653d714d7f25dfed3dcb6
doanphuc281201 <ndp281201@gmail.com>
2022-04-28 11:23:32 +0700

    --new update

--- HDH---Project_1/ReadData.h
+++ HDH---Project_1/ReadData.h
@@ -10,3 +10,4 @@ int hexCharToInt(char a);
 string hexToString(std::string str);
 string ReadtoString(BYTE* data, string offsetHex, unsigned int bytes);
 uint64_t LE_read_reverse( uint8_t* start_byte, string Hex_offset,unsigned int total );   //tên hàm cũ : ReadIntReverse
+void print_txt_data( byte* data); // tên hàm cũ : void printTextData(byte* data, int size);
